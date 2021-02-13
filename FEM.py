2236c0ec1001f662b4f8eb8c457dc8874f7903a5
Johannes Dieterle <46397040+JohannesDieterle@users.noreply.github.com>
2021-02-13 15:45:27 +0100

    main aufgeräumt

---
+++ FEM.py
@@ -0,0 +1,27 @@
+"""File to outsource functions and classes"""
+
+class Baustoff:
+    def __init__(self):
+        self.d = 0  # Dicke, m
+        self.lambdaX = 0  # Wärmeleitfähigkeit, W/(m*K)
+
+def Temperaturanpassung(Punkt, matrix_Punkte, matrix_Elemente):
+    # Formel um die Temperatur eines Punktes mit den Nebenpunkten anzupassen
+    R_0 = matrix_Elemente[Punkt-1][1]/matrix_Elemente[Punkt-1][0]
+    R_1 = matrix_Elemente[Punkt][1]/matrix_Elemente[Punkt][0]
+    return (R_0 * matrix_Punkte[Punkt-1] + R_1 * matrix_Punkte[Punkt+1])/(R_0 + R_1)
+
+def Iteration(Anzahl, matrix_Punkte, matrix_Elemente):
+    # plt.ion()
+    for i in range(Anzahl):
+        matrix_Punkte_tmp = matrix_Punkte.copy()
+        for j in range(len(matrix_Punkte)):
+            if j == 0 or j == len(matrix_Punkte)-1:
+                continue
+            matrix_Punkte[j] = Temperaturanpassung(j, matrix_Punkte_tmp, matrix_Elemente)
+        # plt.plot(x_value,matrix_Punkte)
+        # plt.draw()
+        # plt.pause(0.0001)
+        # plt.clf()
+    return matrix_Punkte
+
--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-# 1D Finite-Elemente-Methode für den Wärmeverlauf in einem Baustoff
+# 2D Finite-Elemente-Methode für den Wärmeverlauf in einem Bauteil
 import matplotlib.pyplot as plt
+import FEM
 
 n = 0  # Anzahl an Elemente
 theta_i = 20  # Innentemperatur, °C
 theta_e = -5  # Außentemperatur, °C
 
-class Baustoff:
-    def __init__(self):
-        self.d = 0  # Dicke, m
-        self.lambdaX = 0  # Wärmeleitfähigkeit, W/(m*K)
-
-B_Holz = Baustoff()
+B_Holz = FEM.Baustoff()
 B_Holz.d = 0.1
 B_Holz.lambdaX = 0.1
 
-B_Beton = Baustoff()
+B_Beton = FEM.Baustoff()
 B_Beton.d = 0.3
 B_Beton.lambdaX = 2.1
 
-B_Dämmung = Baustoff()
+B_Dämmung = FEM.Baustoff()
 B_Dämmung.d = 0.1
 B_Dämmung.lambdaX = 0.035
 
-B_Putz = Baustoff()
+B_Putz = FEM.Baustoff()
 B_Putz.d = 0.01
 B_Putz.lambdaX = 0.7
 
@@ -34,7 +30,7 @@ ges_d = 0
 for item in Bauteil:
     ges_d = ges_d + item.d
 
-# Anzahl der Elemente pro Baustoff
+# Anzahl der Elemente pro FEM.Baustoffe
 n_i = 4
 n = len(Bauteil)*n_i+1
 
@@ -61,27 +57,6 @@ for i in range(n-1):
     matrix_Elemente.append([Bauteil[Bauteil_zeiger].d/n_i, Bauteil[Bauteil_zeiger].lambdaX])
     i2 += 1
 
-def Temperaturanpassung(Punkt, matrix_Punkte, matrix_Elemente):
-    # Formel um die Temperatur eines Punktes mit den Nebenpunkten anzupassen
-    # TODO: "Eins links, eins rechts abwechselnd berechnen"
-    R_0 = matrix_Elemente[Punkt-1][1]/matrix_Elemente[Punkt-1][0]
-    R_1 = matrix_Elemente[Punkt][1]/matrix_Elemente[Punkt][0]
-    return (R_0 * matrix_Punkte[Punkt-1] + R_1 * matrix_Punkte[Punkt+1])/(R_0 + R_1)
-
-def Iteration(Anzahl, matrix_Punkte, matrix_Elemente):
-    # plt.ion()
-    for i in range(Anzahl):
-        matrix_Punkte_tmp = matrix_Punkte.copy()
-        for j in range(len(matrix_Punkte)):
-            if j == 0 or j == len(matrix_Punkte)-1:
-                continue
-            matrix_Punkte[j] = Temperaturanpassung(j, matrix_Punkte_tmp, matrix_Elemente)
-        # plt.plot(x_value,matrix_Punkte)
-        # plt.draw()
-        # plt.pause(0.0001)
-        # plt.clf()
-    return matrix_Punkte
-
 # Plot in der X-Richtung richtig strecken
 x_value=[0.0]
 tmp2 = 0
@@ -89,7 +64,7 @@ for i in range(len(matrix_Elemente)):
     tmp2 += round(matrix_Elemente[i][0], 4)
     x_value.append(round(tmp2, 4))
 
-Iteration(10000, matrix_Punkte, matrix_Elemente)
+FEM.Iteration(10000, matrix_Punkte, matrix_Elemente)
 
 # Temperaturverlauf plotten
 plt.plot(x_value, matrix_Punkte)
